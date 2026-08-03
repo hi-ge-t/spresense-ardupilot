ff33dd38f90f1670d4072c1c43f70bec08a396cf
hi-ge-t <gaoqiaoyongci73@gmail.com>
2026-08-04 08:17:35 +0900

    AP_HAL_Spresense: trace microSD file initialization

--- libraries/AP_HAL_Spresense/SafeBringup.cpp
+++ libraries/AP_HAL_Spresense/SafeBringup.cpp
@@ -12,6 +12,17 @@
 
 namespace Spresense {
 
+#if defined(__NuttX__)
+namespace {
+
+void storage_init_marker(const char *marker)
+{
+    (void)::write(STDOUT_FILENO, marker, strlen(marker));
+}
+
+} // namespace
+#endif
+
 bool MonotonicClock::micros(uint64_t &time_us) const
 {
     struct timespec now {};
@@ -147,13 +158,41 @@ bool StorageFile::init(const char *path, size_t size)
 
     _fd = ::open(path, O_RDWR | O_CREAT | O_CLOEXEC, 0600);
     if (_fd < 0) {
+#if defined(__NuttX__)
+        if (strcmp(path, STORAGE_PATH) == 0) {
+            storage_init_marker(errno == EROFS
+                ? "SPRESENSE_M1_STORAGE=OPEN_EROFS\n"
+                : errno == EACCES
+                    ? "SPRESENSE_M1_STORAGE=OPEN_EACCES\n"
+                    : "SPRESENSE_M1_STORAGE=OPEN_FAIL\n");
+        }
+#endif
         return false;
     }
+#if defined(__NuttX__)
+    if (strcmp(path, STORAGE_PATH) == 0) {
+        storage_init_marker("SPRESENSE_M1_STORAGE=OPEN_OK\n");
+    }
+#endif
     if (ftruncate(_fd, static_cast<off_t>(size)) != 0) {
+#if defined(__NuttX__)
+        if (strcmp(path, STORAGE_PATH) == 0) {
+            storage_init_marker(errno == ENOSPC
+                ? "SPRESENSE_M1_STORAGE=TRUNCATE_ENOSPC\n"
+                : errno == EROFS
+                    ? "SPRESENSE_M1_STORAGE=TRUNCATE_EROFS\n"
+                    : "SPRESENSE_M1_STORAGE=TRUNCATE_FAIL\n");
+        }
+#endif
         close();
         return false;
     }
     _size = size;
+#if defined(__NuttX__)
+    if (strcmp(path, STORAGE_PATH) == 0) {
+        storage_init_marker("SPRESENSE_M1_STORAGE=READY\n");
+    }
+#endif
     return true;
 }
 
