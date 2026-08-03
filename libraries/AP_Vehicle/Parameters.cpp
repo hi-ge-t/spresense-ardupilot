656f3842b97757ca81c46966179aa66d90eb9a58
hi-ge-t <gaoqiaoyongci73@gmail.com>
2026-08-04 07:42:03 +0900

    AP_Param: trace Spresense parameter loading

--- libraries/AP_Vehicle/Parameters.cpp
+++ libraries/AP_Vehicle/Parameters.cpp
@@ -19,10 +19,16 @@ void AP_Vehicle::load_parameters(AP_Int16 &format_version, const uint16_t expect
         format_version.set_and_save(expected_format_version);
         hal.console->printf("done.\n");
     }
+    hal.console->printf("SPRESENSE_M1_PARAMS=ERASE_AFTER\n");
+    hal.console->flush();
     format_version.set_default(expected_format_version);
 
     // Load all auto-loaded EEPROM variables
+    hal.console->printf("SPRESENSE_M1_PARAMS=LOAD_ALL_BEFORE\n");
+    hal.console->flush();
     AP_Param::load_all();
+    hal.console->printf("SPRESENSE_M1_PARAMS=LOAD_ALL_AFTER\n");
+    hal.console->flush();
 }
 
 #endif  // AP_VEHICLE_ENABLED
