e20bd3d21ad2fdc6964e5f44d027df3a96ecca2c
hi-ge-t <gaoqiaoyongci73@gmail.com>
2026-08-03 23:30:22 +0900

    AP_HAL_Spresense: trace static initialization entry

--- Tools/spresense/verify_m1_copter_contract.py
+++ Tools/spresense/verify_m1_copter_contract.py
@@ -133,6 +133,7 @@ def main() -> int:
             "pthread_create",
             "PTHREAD_PRIO_INHERIT",
             'Spresense::UARTDriver serial0_driver("/dev/ttyS0")',
+            'SPRESENSE_M1_COPTER_BOOT=CTORS_BEGIN',
             'SPRESENSE_M1_COPTER_BOOT=HAL',
             'SPRESENSE_M1_COPTER_BOOT=SCHEDULER_FAIL',
             "Empty::I2CDeviceManager i2c_manager",
